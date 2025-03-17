6186124a8236f4d790a594b52bff380adb6f7396
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2025-03-17 22:38:09 +0000

    Beat Vox

--- docs/js/articles-list.js
+++ docs/js/articles-list.js
@@ -1,6 +1,7 @@
 // List of article filenames in chronological order (newest first)
 const articles = [
   // Add more articles here as they're created
+  '2019-06-28-beat_vox.html',
   '2017-06-07-Tap_the_rain.html',
   '2015-11-15-Ind_Ud.html',
 ]
